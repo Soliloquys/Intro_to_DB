a8692ccaa5eb43089cf800119bdf36b8b66b5ea0
Simon Kwabla Azigie <simonziggy18@gmail.com>
2025-10-19 18:44:15 +0000

    new

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,10 +1,34 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
-USE DATABASE alx_book_store;
 
 CREATE TABLE Books (
     book_id INT PRIMARY KEY,
     title VARCHAR(130),
     author_id INT,
     price DOUBLE,
-    publication_date DATE,
-)
+    publication_date DATE
+);
+
+CREATE TABLE Authors (
+    author_id INT,
+    author_name VARCHAR (215)
+);
+
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    customer name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT,
+);
+
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+);
+
+CREATE TABLE Order_Details (
+    orderdetailid INT PRIMARY KEY,
+    book_id INT,
+    order_id INT,
+    quantity DOUBLE,
+);
